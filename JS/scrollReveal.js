25fe63ef7d6b82a9093be67b4218eb5b6db80c4b
Cindy Colin <cindycolin@outlook.be>
2019-01-22 17:32:04 +0100

    scrollreveal faster

--- JS/scrollReveal.js
+++ JS/scrollReveal.js
@@ -5,10 +5,10 @@ window.sr = ScrollReveal();
 sr.reveal('h2', {
 	origin: 'right',
 	distance: '150px',
-	duration: 2000,
+	duration: 1300,
 	scale: 0.2,
 	reset: true,
-	delay: 500,
+	delay: 350,
 	mobile:true
 });
 
@@ -25,80 +25,80 @@ sr.reveal('#intro', {
 sr.reveal('h3', {
 	origin: 'left',
 	distance: '150px',
-	duration: 2000,
+	duration: 1300,
 	scale: 0.2,
 	reset: true,
-	delay: 500,
+	delay: 350,
 	mobile:false
 });
 
 sr.reveal('.underline',{
 	origin: 'right',
 	distance: '950px',
-	duration: 2000,
+	duration: 1300,
 	scale: 0.2,
 	reset: true,
-	delay: 500,
+	delay: 350,
 	mobile:true
 });
 sr.reveal('.underline3a',{
 	origin: 'right',
 	distance: '950px',
-	duration: 2000,
+	duration: 1300,
 	scale: 0.2,
 	reset: true,
-	delay: 500,
+	delay: 350,
 	mobile:true
 });
 sr.reveal('.underlineBis',{
 	origin: 'right',
 	distance: '950px',
-	duration: 2000,
+	duration: 1300,
 	scale: 0.2,
 	reset: true,
-	delay: 500,
+	delay: 350,
 	mobile:false
 });
 
 sr.reveal('.underline2Bis',{
 	origin: 'left',
 	distance: '950px',
-	duration: 2000,
+	duration: 1300,
 	scale: 0.2,
 	reset: true,
-	delay: 500
+	delay: 350
 });
 sr.reveal('.underline3',{
 	origin: 'left',
 	distance: '950px',
-	duration: 2000,
+	duration: 1300,
 	scale: 0.2,
 	reset: true,
-	delay: 500
+	delay: 350
 });
 sr.reveal('.underline2', {
 	origin: 'left',
 	distance: '950px',
-	duration: 2000,
+	duration: 1300,
 	scale: 0.2,
 	reset: true,
-	delay: 500
+	delay: 350
 });
 
 sr.reveal('.skills',{
 	origin: 'bottom',
 	distance: '200px',
-	duration: 2000,
+	duration: 1300,
 	reset: true,
-	delay: 250
+	delay: 350
 });
 sr.reveal('.underline4', {
 	origin: 'bottom',
 	distance: '150px',
-	duration: 2000,
+	duration: 1300,
 	scale: 0.2,
 	reset: true,
-	delay: 500
+	delay: 350
 });
 //sr.reveal('#section5',{
 //	origin: 'top',
